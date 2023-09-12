5b4f9cdb620fdcd4fec8276bb428accbbc4e17bc
olurocks <olurocks1@gmail.com>
2023-09-12 07:16:43 +0100

    update sql details

--- config/database.js
+++ config/database.js
@@ -1,13 +1,15 @@
 const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
 
-// Replace the following values with your MySQL database credentials
-const sequelize = new Sequelize('crud_api', 'root', 't3rm1nat0r', {
-  host: 'localhost',
-  dialect: 'mysql', // or 'postgresql', 'sqlite', 'mariadb'
-  port: 3306, // Port for MySQL
-  logging: false, // Set to true to log SQL queries (optional)
+// Load environment variables from .env file
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+  port: process.env.DB_PORT,
+  logging: process.env.DB_LOGGING === 'true', // Convert string to boolean
   dialectModule: require('mysql2'),
-
 });
 
 module.exports = sequelize;
